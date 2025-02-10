7ce5326257ef960b2cfc589fcbaa2bb52c154e54
LearningDestiny <your-email@example.com>
2025-02-10 13:59:18 +0530

    Adjusted categories section styling

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -11,18 +11,18 @@ const Filter = ({ selectedCategories, handleCategoryChange }) => {
   ]
 
   return (
-    <div className="p-4 rounded-lg w-56">
-      <h4 className="text-black font-bold mb-4 text-xl -ml-16">Categories</h4>
+    <div className="p-4 rounded-lg w-56 ml-6">
+     <h4 className="text-black font-bold mb-4 text-lg">Categories</h4>
       {categories.map((category) => (
-        <div key={category} className="flex items-center mb-6 -ml-20 mt-7">
+        <div key={category} className="flex items-center mb-4 mt-4">
           <input
             type="checkbox"
             id={category}
             checked={selectedCategories.includes(category)}
             onChange={() => handleCategoryChange(category)}
-            className="mr-3 w-5 h-5" 
+            className="mr-3 w-4 h-4"
           />
-          <label htmlFor={category} className="text-black text-lg"> 
+          <label htmlFor={category} className="text-black text-base">
             {category}
           </label>
         </div>
