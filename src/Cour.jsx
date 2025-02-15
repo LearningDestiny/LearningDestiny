de9b543f916cc9fcabd9606d94474073e96e701f
LearningDestiny <your-email@example.com>
2025-02-16 02:07:15 +0530

    Reduced gap between cards and adjust in center

--- src/Cour.jsx
+++ src/Cour.jsx
@@ -213,7 +213,7 @@ const Courses = () => {
 
               {/* Container: Enables horizontal scrolling on mobile, grid layout on desktop */}
               {/* <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 sm:gap-4"> */}
-              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1">
+              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-2 gap-1">
                 {filteredCourses.map((course) => (
                   <div
                     key={course.id}
