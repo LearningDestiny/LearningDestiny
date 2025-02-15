d4505382f528810200a3664d5abbc90a5be343dd
LearningDestiny <your-email@example.com>
2025-02-15 18:50:43 +0530

    Adjusted course card size

--- src/Cour.jsx
+++ src/Cour.jsx
@@ -219,6 +219,7 @@ const Courses = () => {
                       course={course}
                       isHovered={hoveredAllCourse}
                       setHovered={setHoveredAllCourse}
+                      className="scale-90 sm:scale-100"
                     />
                   </div>
                 ))}
