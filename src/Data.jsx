98e4eff3eef777157aae639125c471f7fabe29a2
LearningDestiny <devabathinipranavchowdary@gmail.com>
2025-02-17 00:52:52 +0530

    Update Data

--- src/Data.jsx
+++ src/Data.jsx
@@ -8,7 +8,7 @@ export const courses = [
     rating: 4.1,
     ratingCount: 75,
     price: "6,999 Rs",
-    imageUrl: "./CourseID1.jpeg",
+    img src:"./CourseID1.jpeg" alt="CourseIID1",
     lastUpdated: "June 2024",
     duration: "80 hours",
     lectureCount: 15,
